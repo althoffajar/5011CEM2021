ba2f0fb8641896200c0e20067630e65c2996f7d8
althoffajar <83429870+althoffajar@users.noreply.github.com>
2021-04-30 11:48:31 +0100

    Delete Project directory

--- Project/sequentialProcessing/Manual Testing/BLoadParameters.m
+++
@@ -1,4 +0,0 @@
-%% Processing parameters provided by customer
-RadLat = 30.2016; % cluster radius value for latitude
-RadLon = 24.8032; % cluster radius value for longitude
-RadO3 = 4.2653986e-08; % cluster radius value for the ozone data
